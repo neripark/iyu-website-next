5fa45507764f8d656411540e69d03462b31808bf
neripark <neripark@gmail.com>
2022-11-14 16:53:52 +0900

    init MainVisual (except sp)

---
+++ src/templates/MainVisual/index.tsx
@@ -0,0 +1,23 @@
+import styles from "./style.module.scss";
+
+export const MainVisual: React.FC = () => {
+  return (
+    <section className={styles["main-visual"]}>
+      <h1 className={styles["iyu-logo"]}>
+        <img src="/assets/images/iyu-logo-RGB.svg" alt="iyu-logo" />
+      </h1>
+      {/* todo: */}
+      {/* <information-summary :live-details="liveDetails" /> */}
+      {/* todo: PCのときだけ動画をロードできているか確認 */}
+      {typeof window !== undefined && (
+        <video
+          className={styles["bg-movie"]}
+          src="/assets/videos/200109_iyu-webtop-video.mp4"
+          muted
+          autoPlay
+          loop
+        />
+      )}
+    </section>
+  );
+};
---
+++ src/templates/index.tsx
@@ -0,0 +1,5 @@
+import { MainVisual } from "./MainVisual";
+
+export const Template: React.FC = () => {
+  return <MainVisual />;
+};
