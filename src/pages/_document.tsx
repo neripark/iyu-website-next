7642f56eef091b8c86bfd54aa42d213b02854868
neripark <neripark@gmail.com>
2022-11-16 18:38:51 +0900

    lint @next/next/no-page-custom-font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        {/* note: https://nextjs.org/docs/messages/no-page-custom-font の対策のためここに書いている */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic&display=optional"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Heebo:700&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
