7e9540fa9d8409cc320acddaae0bd9a97686c1f3
neripark <neripark@gmail.com>
2022-12-02 18:19:33 +0900

    add todo comment

--- src/templates/Contact/Form/CategorySelect/hooks.ts
+++ src/templates/Contact/Form/CategorySelect/hooks.ts
@@ -4,10 +4,12 @@ import { UserInputContext } from "../UserInputProvider";
 export const useHooks = () => {
   const { userInput } = useContext(UserInputContext);
 
+  // todo: Context 側に寄せる
   const isSelectedLiveReserve = useMemo(() => {
     return userInput.category === "live";
   }, [userInput.category]);
 
+  // todo: Context 側に寄せる
   const isSelectedSomeCategory = useMemo(() => {
     return userInput.category !== undefined;
   }, [userInput.category]);
