fe4092d8d09f785f44a93ad90de494ade615b558
neripark <neripark@gmail.com>
2022-12-18 18:01:44 +0900

    use custom arrows

--- src/templates/Gallery/Carousel/index.tsx
+++ src/templates/Gallery/Carousel/index.tsx
@@ -1,4 +1,4 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/splide/css";
 import { images } from "./images";
 import styles from "./style.module.scss";
@@ -10,6 +10,7 @@ interface Props {
 export const Carousel: React.FC<Props> = (props) => (
   <Splide
     aria-label="私のお気に入りの画像集"
+    hasTrack={false}
     options={{
       classes: {
         pagination: `splide__pagination ${styles["custom__pagination"]}`,
@@ -25,6 +26,7 @@ export const Carousel: React.FC<Props> = (props) => (
         700: {
           fixedWidth: "100%",
           padding: 30,
+          arrows: false,
         },
         600: {
           padding: 15,
@@ -35,17 +37,40 @@ export const Carousel: React.FC<Props> = (props) => (
     }}
     className={props.className}
   >
-    {images.map((element) => (
-      // todo: カルーセルできていけそうなら next/image を使う
-      <SplideSlide key={element.src}>
+    <SplideTrack>
+      {images.map((element) => (
+        // todo: カルーセルできていけそうなら next/image を使う
+        <SplideSlide key={element.src}>
+          <img
+            alt={element.alt}
+            className={styles["image"]}
+            loading="lazy"
+            src={element.src}
+          />
+        </SplideSlide>
+      ))}
+    </SplideTrack>
+    {/* note: カスタマイズする場合は `<SplideTrack />` と描画位置を指定するための div.splide__arrows が必要 */}
+    <div className="splide__arrows">
+      <button
+        className={`splide__arrow ${styles["custom__arrow"]} splide__arrow--prev ${styles["custom__arrow--prev"]}`}
+      >
         <img
-          alt={element.alt}
-          className={styles["image"]}
-          loading="lazy"
-          src={element.src}
+          className={styles["arrow-image"]}
+          src="/assets/images/gallery-button-L.png"
+          alt="to Previous Slide"
         />
-      </SplideSlide>
-    ))}
+      </button>
+      <button
+        className={`splide__arrow ${styles["custom__arrow"]} splide__arrow--next ${styles["custom__arrow--next"]}`}
+      >
+        <img
+          className={styles["arrow-image"]}
+          src="/assets/images/gallery-button-R.png"
+          alt="to Next Slide"
+        />
+      </button>
+    </div>
     {/* note:
         アクティブなページネーションボタンのみにclassを渡すことができないため、 module.css で scoped にできない。
         scoped ではないCSSファイルを読み込もうとすると、 `App.tsx` 以外の場所から読み込まないでねという趣旨のエラーが
