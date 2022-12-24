3ff99f9d079ee7385436e6c74c94584b262933cc
neripark <neripark@gmail.com>
2022-12-24 11:58:54 +0900

    add new section

---
+++ src/templates/Goods/index.tsx
@@ -0,0 +1,12 @@
+import { Heading } from "@/components/Heading";
+import { anchorList } from "@/constants/anchorList";
+import styles from "./style.module.scss";
+
+export const Goods: React.FC = () => {
+  return (
+    <section className={styles["root"]} id={anchorList.goods.id}>
+      {/* todo: Heading のテキスト色を確認する */}
+      <Heading color="blue">{anchorList.goods.label}</Heading>
+    </section>
+  );
+};
