45e845ca3aa787632b833eb72d54af5a8c358952
neripark <neripark@gmail.com>
2023-01-05 12:32:19 +0900

    simple replace

--- src/templates/MainVisual/index.tsx
+++ src/templates/MainVisual/index.tsx
@@ -1,4 +1,5 @@
 import { anchorList } from "@/constants/anchorList";
+import { IyuLogo } from "@/components/IyuLogo";
 import Image from "next/image";
 import { InformationSummary } from "./InformationSummary";
 import styles from "./style.module.scss";
@@ -7,13 +8,14 @@ export const MainVisual: React.FC = () => {
   return (
     <section className={styles["main-visual"]} id={anchorList.mainVisual.id}>
       <h1 className={styles["heading"]}>
-        <Image
+      <IyuLogo />
+        {/* <Image
           alt="iyu-logo"
           className={styles["iyu-logo"]}
           height={150}
           src="/assets/images/iyu-logo-RGB.svg"
           width={267}
-        />
+        /> */}
       </h1>
       <InformationSummary />
       <video
