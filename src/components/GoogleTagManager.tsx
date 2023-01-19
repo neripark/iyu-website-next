0dd5480f454ae75ca4defc74da6f778e3d427ede
neripark <neripark@gmail.com>
2023-01-19 12:13:01 +0900

    refactor

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -4,20 +4,18 @@ interface Props {
   containerId: string;
 }
 
-export const GoogleTagManager: React.FC<Props> = (props) => {
-  return (
-    <Script
-      id="gtm"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
+export const GoogleTagManager: React.FC<Props> = (props) => (
+  <Script
+    id="gtm"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','${props.containerId}');
       `,
-      }}
-    />
-  );
-};
+    }}
+  />
+);
