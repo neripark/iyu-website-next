186f199bb13555904a6cbbfd4ea425614958bc4a
neripark <neripark@gmail.com>
2023-06-28 12:04:14 +0900

    run format

--- src/templates/Template.stories.ts
+++ src/templates/Template.stories.ts
@@ -1,13 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Template } from '.';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Template } from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Template,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 } satisfies Meta<typeof Template>;
 
