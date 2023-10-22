f89ed7a57eefab7f327fb7d5c59ef1b79fe96132
neripark <neripark@gmail.com>
2023-10-22 17:22:26 +0900

    improve test

--- src/api/receiveContactForm/validate/index.test.ts
+++ src/api/receiveContactForm/validate/index.test.ts
@@ -15,43 +15,86 @@ const validDataLive: SelectedLive = {
 };
 
 describe("validate", () => {
-  describe("validな場合", () => {
-    test("パスすること", () => {
-      const data: ContactFormItem = validDataLive;
-      expect(validate(data)).toEqual(data);
+  describe("カテゴリがLiveの場合", () => {
+    describe("validなデータ", () => {
+      test("パスすること", () => {
+        const data: ContactFormItem = validDataLive;
+        expect(validate(data)).toEqual(data);
+      });
+      test("名前の前後にスペースが入力されている場合、スペースが取り除かれて返却されること", () => {
+        const data: ContactFormItem = {
+          ...validDataLive,
+          name: "  test neripark　",
+        };
+        expect(validate(data)).toEqual({ ...data, name: "test neripark" });
+      });
     });
-    test("名前の前後にスペースが入力されている場合、スペースが取り除かれて返却されること", () => {
-      const data: ContactFormItem = {
-        ...validDataLive,
-        name: "  test neripark　",
-      };
-      expect(validate(data)).toEqual({ ...data, name: "test neripark" });
-    });
-  });
 
-  describe("invalidな場合", () => {
-    test("名前が空の場合、例外が投げられること", () => {
-      const data: ContactFormItem = {
-        ...validDataLive,
-        name: "",
-      };
-      expect(() => validate(data)).toThrowError("1文字以上必要です。");
-    });
-    test("名前がスペースのみで入力されている場合、例外が投げられること", () => {
-      const data: ContactFormItem = {
-        ...validDataLive,
-        name: " ",
-      };
-      expect(() => validate(data)).toThrowError("1文字以上必要です。");
-    });
-    test("emailが不正な形式の場合、例外が投げられること", () => {
-      const data: ContactFormItem = {
-        ...validDataLive,
-        email: "aaa",
-      };
-      expect(() => validate(data)).toThrowError(
-        "メールアドレスの形式が不正です。",
-      );
+    describe("invalidなデータ", () => {
+      test("名前が空の場合、例外が投げられること", () => {
+        const data: ContactFormItem = {
+          ...validDataLive,
+          name: "",
+        };
+        expect(() => validate(data)).toThrowError("1文字以上必要です。");
+      });
+      test("名前がスペースのみで入力されている場合、例外が投げられること", () => {
+        const data: ContactFormItem = {
+          ...validDataLive,
+          name: " ",
+        };
+        expect(() => validate(data)).toThrowError("1文字以上必要です。");
+      });
+      test("emailが不正な形式の場合、例外が投げられること", () => {
+        const data: ContactFormItem = {
+          ...validDataLive,
+          email: "aaa",
+        };
+        expect(() => validate(data)).toThrowError(
+          "メールアドレスの形式が不正です。",
+        );
+      });
+      test("messageが空の場合、例外が投げられること", () => {
+        const data: ContactFormItem = {
+          ...validDataLive,
+          message: "",
+        };
+        expect(() => validate(data)).toThrowError("1文字以上必要です。");
+      });
+      test("reservedateが空の場合、例外が投げられること", () => {
+        const data: ContactFormItem = {
+          ...validDataLive,
+          reservedate: "",
+        };
+        expect(() => validate(data)).toThrowError("1文字以上必要です。");
+      });
+      test("reservecountが空の場合、例外が投げられること", () => {
+        const data: ContactFormItem = {
+          ...validDataLive,
+          reservecount: "",
+        };
+        expect(() => validate(data)).toThrowError("1文字以上必要です。");
+      });
+      test("reservedateのフィールドが存在していない場合、例外が投げられること", () => {
+        const data = {
+          category: validDataLive.category,
+          name: validDataLive.name,
+          email: validDataLive.email,
+          message: validDataLive.message,
+          reservecount: validDataLive.reservecount,
+        };
+        expect(() => validate(data as any)).toThrowError();
+      });
+      test("reservecountのフィールドが存在していない場合、例外が投げられること", () => {
+        const data = {
+          category: validDataLive.category,
+          name: validDataLive.name,
+          email: validDataLive.email,
+          message: validDataLive.message,
+          reservedate: validDataLive.reservedate,
+        };
+        expect(() => validate(data as any)).toThrowError();
+      });
     });
   });
 });
