7f945398bbd28b2662ca9951582508552614b81c
neripark <neripark@gmail.com>
2025-03-29 20:18:37 +0900

    improve type annotation

--- src/types/node.d.ts
+++ src/types/node.d.ts
@@ -5,7 +5,8 @@ declare namespace NodeJS {
     readonly APP_ENV: string;
     readonly CONTENTFUL_SPACE_ID: string;
     readonly CONTENTFUL_ACCESS_TOKEN: string;
-    readonly LINE_NOTIFY_TOKEN: string;
+    readonly LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN: string;
+    readonly LINE_GROUP_ID: string;
     readonly GOOGLE_TAG_MANAGER_CONTAINER_ID: string;
   }
 }
