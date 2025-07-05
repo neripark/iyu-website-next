c49bbb5de08c236d3abc35c267cbb180d579bc8c
neripark <neripark@gmail.com>
2025-07-05 18:01:14 +0900

    run format

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -2,15 +2,15 @@
 export const Meta: React.FC = () => {
   return (
     <>
-        {/* note: https://nextjs.org/docs/messages/no-page-custom-font の対策のためここに書いている */}
-        <link
-          href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic&display=optional"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Heebo:700&display=swap"
-          rel="stylesheet"
-        />
+      {/* note: https://nextjs.org/docs/messages/no-page-custom-font の対策のためここに書いている */}
+      <link
+        href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic&display=optional"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Heebo:700&display=swap"
+        rel="stylesheet"
+      />
     </>
   );
 };
